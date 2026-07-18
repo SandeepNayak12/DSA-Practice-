f5a9ad1d6e2a9d82d6f088a53afea8a1523059ca
Sandeep Nayak <sandeepnayak3483@gmail.com>
2026-07-18 23:53:59 +0530

    Time: 8 ms (21.03%), Space: 19.4 MB (23.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,10 +5,9 @@ class Solution:
         high = x
         while low<=high:
             mid= (low+high)//2
-            if mid*mid == x:
-                return mid
-            elif mid*mid>x:
-                high = mid -1
-            else:
+
+            if mid*mid<=x:
                 low = mid+1
+            else:
+                high = mid -1
         return high
